a351a6a746e685a2abb832de0577f139393c43da
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2025-04-10 21:41:08 +0900

    00_01の内容をまとめた

---
+++ MyMath.cpp
@@ -0,0 +1,90 @@
+﻿#include "MyMath.h"
+
+
+/*-----------------------------------------------------------------------*/
+//
+//								2次元ベクトル
+//
+/*-----------------------------------------------------------------------*/
+
+
+
+
+
+
+
+
+/*-----------------------------------------------------------------------*/
+//
+//								3次元ベクトル
+//
+/*-----------------------------------------------------------------------*/
+
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	static const int kColumnWidthVec3 = 60;
+	static const int kRowHeightVec3 = 20;
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidthVec3, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidthVec3 * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidthVec3 * 3, y, "%s", label);
+};
+
+Vector3 Vector3Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result = {
+		v1.x + v2.x,
+		v1.y + v2.y,
+		v1.z + v2.z
+	};
+	return result;
+}
+
+Vector3 Vector3Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 result = {
+	v1.x - v2.x,
+	v1.y - v2.y,
+	v1.z - v2.z
+	};
+	return result;
+}
+
+Vector3 Vector3Multiply(const Vector3& v,float scalar ) {
+	Vector3 result = {
+	v.x * scalar,
+	v.y * scalar,
+	v.z * scalar
+	};
+	return result;
+}
+
+float Vector3Dot(const Vector3& v1, const Vector3& v2) {
+	float result =
+		v1.x * v2.x +
+		v1.y * v2.y +
+		v1.z * v2.z;
+
+	return result;
+}
+
+float Vector3Length(const Vector3& v) {
+	float result = sqrtf(
+		v.x * v.x +
+		v.y * v.y +
+		v.z * v.z
+	);
+	return result;
+}
+
+Vector3 Vector3Normalize(const Vector3& v) {
+	Vector3 result = { 0,0,0 };
+	float length = Vector3Length(v);
+
+	if (length != 0) {
+		result.x = v.x / length;
+		result.y = v.y / length;
+		result.z = v.z / length;
+	}
+
+	return result;
+}
+
---
+++ MyMath.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#define _USE_MATH_DEFINES
+#include<math.h>
+#include<Novice.h>
+
+
+/*-----------------------------------------------------------------------*/
+//
+//								2次元ベクトル
+//
+/*-----------------------------------------------------------------------*/
+
+/// <summary>
+/// 2次元ベクトル
+/// </summary>
+struct Vector2 final {
+	float x;
+	float y;
+};
+
+/*-----------------------------------------------------------------------*/
+//
+//								3次元ベクトル
+//
+/*-----------------------------------------------------------------------*/
+/// <summary>
+/// 3次元ベクトル
+/// </summary>
+struct Vector3 final {
+	float x;
+	float y;
+	float z;
+};
+
+//３次元ベクトルの値を表示する
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+//加算
+Vector3 Vector3Add(const Vector3& v1, const Vector3& v2);
+//減算
+Vector3 Vector3Subtract(const Vector3& v1, const Vector3& v2);
+//スカラー倍
+Vector3 Vector3Multiply(const Vector3& v, float scalar);
+//内積
+float Vector3Dot(const Vector3& v1, const Vector3& v2);
+//長さ
+float Vector3Length(const Vector3& v);
+//正規化
+Vector3 Vector3Normalize(const Vector3& v);
+
+
