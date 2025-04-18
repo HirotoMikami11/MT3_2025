ba33950bec3c016a96bdd313a834d26a5f4c9a16
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2025-04-18 12:30:21 +0900

    点と線の距離を求められた

--- main.cpp
+++ main.cpp
@@ -4,6 +4,43 @@
 
 const char kWindowTitle[] = "LE2A_15_ミカミ_ヒロト_MT3_02_00";
 
+/// <summary>
+/// 直線
+/// </summary>
+struct Line {
+	Vector3 origin;		//始点
+	Vector3 diff;		//終点への差分ベクトル
+};
+
+/// <summary>
+/// 半直線
+/// </summary>
+struct Ray {
+	Vector3 origin;		//始点
+	Vector3 diff;		//終点への差分ベクトル
+};
+
+/// <summary>
+/// 線分
+/// </summary>
+struct Segment {
+	Vector3 origin;		//始点
+	Vector3 diff;		//終点への差分ベクトル
+};
+
+//	正射影ベクトルを求める関数
+//	
+Vector3 Project(const Vector3& v1, const Vector3& v2) {
+	Vector3 project = Vector3Multiply(Vector3Normalize(v2), Vector3Dot(v1, Vector3Normalize(v2)));
+	return project;
+}
+//　最近接点を求める関数
+Vector3 ClosestPoint(const Vector3& point, const Segment& segment) {
+	Vector3 project = Project(Vector3Subtract(point,segment.origin),segment.diff);
+	Vector3 closestPoint =Vector3Add(segment.origin,project);
+
+	return closestPoint;
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -20,6 +57,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
 	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
+
+	//計算に使用する関数
+	Segment segment{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
+	Vector3 point{ -1.5f,0.6f,0.6f };
+
+	Vector3 project = Project(Vector3Subtract(point, segment.origin), segment.diff);
+	Vector3 closestPoint = ClosestPoint(point, segment);
+
+	//描画に使用する関数
+	Sphere pointSphere{ point,0.01f };		//1cmの球を描画
+	Sphere closestPointSphere{ closestPoint,0.01f };		//1cmの球を描画
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -39,6 +87,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 viewMatrix = Matrix4x4Inverse(cameraMatrix);
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+		Matrix4x4 viewProjectionMatrix = Matrix4x4Multiply(viewMatrix, projectionMatrix);
+
+		
+		project = Project(Vector3Subtract(point, segment.origin), segment.diff);
+		closestPoint = ClosestPoint(point, segment);
+		//描画に使用する関数
+		pointSphere={ point,0.01f };		//1cmの球を描画
+		closestPointSphere={ closestPoint,0.01f };		//1cmの球を描画
+
+		//線分の両端をそれぞれスクリーン座標に変換
+		Vector3 start = Transform(Transform(segment.origin, viewProjectionMatrix), viewportMatrix);
+		Vector3 end = Transform(Transform(Vector3Add(segment.origin, segment.diff), viewProjectionMatrix), viewportMatrix);
+
+
 
 		///
 		/// ↑更新処理ここまで
@@ -48,14 +110,41 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		DrawGrid(Matrix4x4Multiply(viewMatrix, projectionMatrix), viewportMatrix);
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
 
 		ImGui::Begin("Window");
 		//カメラ
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		
+		//カメラを初期状態に戻す
+		if (ImGui::Button("Reset Camera")) {
+			cameraTranslate={ 0.0f,1.9f,-6.49f };
+			cameraRotate={ 0.26f,0.0f,0.0f };
+		}
+		
+		//線分への正射影ベクトルの結果
+		ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+
+		ImGui::DragFloat3("point", &point.x, 0.01f);
+		ImGui::DragFloat3("segment.origin", &segment.origin.x, 0.01f);
+		ImGui::DragFloat3("segment.diff", &segment.diff.x, 0.01f);
+
 
 		ImGui::End();
+
+
+		//球の描画
+		DrawSphere(pointSphere, viewProjectionMatrix, viewportMatrix, RED);
+		DrawSphere(closestPointSphere, viewProjectionMatrix, viewportMatrix, BLACK);
+
+		//線分の描画
+		Novice::DrawLine(
+			static_cast<int>(start.x),
+			static_cast<int>(start.y),
+			static_cast<int>(end.x),
+			static_cast<int>(end.y),
+			WHITE);
 
 
 		///
