0fefb3f923b56a74d4a989570c11bcad4370dcf7
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2025-05-05 23:15:59 +0900

    線分と平面の当たり判定ができた

--- main.cpp
+++ main.cpp
@@ -5,6 +5,33 @@
 
 const char kWindowTitle[] = "LE2A_15_ミカミ_ヒロト_MT3_02_03";
 
+
+bool IsCollision(const Segment& segment, const Plane& plane) {
+	//衝突しているかどうか
+	bool isCollision = false;
+
+	//垂直判定を行うため、法線と線の内積を求める
+	float dot = Vector3Dot(plane.normal, segment.diff);
+	//垂直=平行であるので、衝突していない
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	//tを求める
+	float t = (plane.distance - Vector3Dot(segment.origin, plane.normal)) / dot;
+
+	//tの値と線の種類によって衝突しているかを判定する
+	//線分なので0~1
+	if (t >= 0 && t <= 1) {
+		isCollision = true;
+	}
+
+	return isCollision;
+}
+
+
+
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -20,6 +47,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 	Plane plane_ = { {0,1,0},1.0f };
+	Segment segment_ = { 
+		{-0.45f,0.7f,0.0f},	//始点
+		{1.0f,0.5f,0.0f}	//終点
+	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -41,6 +72,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -50,18 +82,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		ImGui::Begin("Window");
+		ImGui::SliderFloat3("plane_Normal", &plane_.normal.x, -1.0f, 1.0f);
+		plane_.normal = Vector3Normalize(plane_.normal);
+		ImGui::SliderFloat("plane_Distance", &plane_.distance, 0.0f, 1.0f);
 
-		ImGui::SliderFloat3("Plane Center", &plane_.normal.x, -1.0f, 1.0f);
-
-		ImGui::SliderFloat("Plane Radius", &plane_.distance, 0.0f, 1.0f);
-
-
+		ImGui::SliderFloat3("segment_Origin", &segment_.origin.x, 0.0f, 1.0f);
+		ImGui::SliderFloat3("segment_Diff", &segment_.diff.x, 0.0f, 1.0f);
 		ImGui::End();
 
 		//グリッド線を描画
 		DrawGrid(camera->GetViewProjectionMatrix(), camera->GetViewportMatrix());
-
 		DrawPlane(plane_, camera->GetViewProjectionMatrix(), camera->GetViewportMatrix(), WHITE);
+		//ライン
+		DrawLine(segment_, camera->GetViewProjectionMatrix(), camera->GetViewportMatrix(), IsCollision(segment_, plane_) ? RED : WHITE);
 
 		///
 		/// ↑描画処理ここまで
